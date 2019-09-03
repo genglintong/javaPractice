38d3d93f040ed1561b3da1ff7c20a060e0deac6f
genglintong <genglintong@100tal.com>
2019-09-03 12:41:51 +0800

    add med 054

--- src/com/company/medium/_054/Solution.java
+++ src/com/company/medium/_054/Solution.java
@@ -1,7 +1,6 @@
 package com.company.medium._054;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -13,15 +12,44 @@ import java.util.List;
 public class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> list = new ArrayList<>();
-        int n = matrix.length;
-        if (n <= 0) {
+        int mR = matrix.length;
+
+        if (mR == 0) {
             return list;
         }
-        int t = n / 2;
-        return Collections.emptyList();
+
+        int mC = matrix[0].length;
+        boolean[][] seen = new boolean[mR][mC];
+        int[] dr = {0,1,0,-1};
+        int[] dc = {1,0,-1,0};
+        int r = 0, c = 0 , di = 0;
+
+        for (int i = 0 ; i < mR * mC ; i++) {
+            //System.out.println(r + " " + c);
+            list.add(matrix[r][c]);
+            seen[r][c] = true;
+
+            int cr = r + dr[di], cc = c + dc[di];
+            if (cr >= 0 && cr < mR && cc >= 0 && cc < mC && !seen[cr][cc]) {
+                r = cr;
+                c = cc;
+            }else {
+                di = (di + 1) % 4;
+                r += dr[di];
+                c += dc[di];
+            }
+        }
+        return list;
     }
 
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        System.out.println(solution.spiralOrder(new int[][]{
+                {1,2,3,4,5},
+                {1,2,3,4,5},
+                {1,2,3,4,5},
+                {1,2,3,4,5},
+                {1,2,3,4,5}
+        }));
     }
 }
