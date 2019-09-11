448495f833c7eda3b2caf9ffb0717ddc2b56442b
genglintong <genglintong@100tal.com>
2019-09-11 19:43:45 +0800

    add mid 081

--- src/com/company/medium/_081/Solution.java
+++ src/com/company/medium/_081/Solution.java
@@ -16,17 +16,36 @@ public class Solution {
             return false;
         }
         int start = 0 , end = len - 1,  mid;
-        while (start < end) {
+        while (start <= end) {
             mid = (start + end) / 2;
-            if (nums[mid] == target) {
+            if (nums[mid] == target || nums[start] == target || nums[end] == target) {
                 return true;
             }
-            //if (nums[start] )
+
+            if (nums[start] == nums[mid]) {
+                start++;
+                continue;
+            }
+            // 前半部分有序
+            if (nums[start] < nums[mid]) {
+                if (nums[start] < target && nums[mid] > target) {
+                    end = mid - 1;
+                }else {
+                    start = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && nums[end] > target) {
+                    start = mid + 1;
+                }else {
+                    end = mid - 1;
+                }
+            }
         }
-        return true;
+        return false;
     }
 
     public static void main(String[] args) {
-
+        Solution solution = new Solution();
+        System.out.println(solution.search(new int[]{2,5,6,0,0,1,2}, 0));
     }
 }
